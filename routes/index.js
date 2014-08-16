0c54c111d3c24763e11d0d5d547b1820e027322a
Stan Dyro <stand@pk4media.com>
2014-08-16 01:25:36 -0700

    maxmind

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var geoip = require('geoip-lite');
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { header: 'Stan Dyro', title: 'standyro' });
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  var geo = geoip.lookup(ip);
+
+  res.render('index', { header: 'Stan Dyro', title: 'standyro', geo: geo, ip: ip });
 });
 
 module.exports = router;
