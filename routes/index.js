4565c50b00f2d6e7c0cc0329c01048986372c7bd
Stan Dyro <stand@pk4media.com>
2014-08-16 01:32:09 -0700

    remove forwarded for unless behind proxy

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var geoip = require('geoip-lite');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  var ip = req.connection.remoteAddress; // req.headers['x-forwarded-for']
   var geo = geoip.lookup(ip);
 
   res.render('index', { header: 'Stan Dyro', title: 'standyro', geo: geo, ip: ip });
