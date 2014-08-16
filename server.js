d0c50241eb251a9f01d06ae355ba6f6b34a02890
Stan Dyro <standyro@gmail.com>
2014-08-16 04:16:41 +0000

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+var sys = require( "sys" );
+var http = require( "http" );
+
+var server = http.createServer(
+function( request, response ){
+
+response.writeHead( 200, {"content-type": "text/plain"} );
+response.write( "Hellow world from AWS!\n" );
+response.end();
+ 
+ 
+}
+);
+
+server.listen( 8080 );
+
+
+sys.puts( "Server is running on 8080" );
