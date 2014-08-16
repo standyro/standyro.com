72fbe2c5f469ee634b82e8550a9e3ed18f91a4bb
Stan Dyro <stand@pk4media.com>
2014-08-15 23:11:43 -0700

    layout change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { header: 'Blog', title: 'standyro' });
+  res.render('index', { header: 'Stan Dyro', title: 'standyro' });
 });
 
 module.exports = router;
