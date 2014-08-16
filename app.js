f69bb5b6831606a982d9dfb922c3e40442044f0c
Stan Dyro <stand@pk4media.com>
2014-08-15 23:23:51 -0700

    port 3000

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
 app.set('view engine', 'handlebars');
 
+app.set('port', process.env.PORT || 3000);
+
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
